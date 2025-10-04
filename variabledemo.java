d29b22e421afc1517a804d0b41d8a420b1fd134e
rohithkottur10-k <rohithkottur10@gmail.com>
2025-10-04 20:32:55 +0530

    variablesclass 10 09 25

---
+++ variabledemo.java
@@ -0,0 +1,13 @@
+  class variabledemo {
+    public static void main(String[]args) {
+        // assinging data //
+       int price = 1000;
+       double rating = 4.5;
+       String brand = "nike"; 
+       // retreving data //
+       System.out.println("price="+ price);
+       System.out.println("rating="+rating);
+       System.out.println("brand="+brand);
+
+    }
+}
