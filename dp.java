1c6557e437714dbe066f2d815d6bda3c7837c930
rohithkottur10-k <rohithkottur10@gmail.com>
2025-10-07 15:43:07 +0530

    data types code

--- dp.java
+++ dp.java
@@ -1,9 +1,23 @@
 class dp{
-    public static void main( String [] args){
+    public static void main(String [] args){
+        byte b=4;
+        short s=999;
+        long l=99999;
+        int i=999999;
 
-        byte b=98;
+        float f=3.5f;
+        double d=34.87413;
+
+        char c='#';
+        boolean boo=true;
+
         System.out.println(b);
-
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(boo);
     }
-
 }
