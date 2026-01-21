5bf888a077623610a5a0a619ffcb9c2775abb345
rohithkottur10-k <rohithkottur10@gmail.com>
2026-01-21 15:07:32 +0530

    11 09 25 conditional statements calss

---
+++ constructors/conditional.java
@@ -0,0 +1,48 @@
+public class conditional {
+    public static void main (String [] args){
+
+        // if statement // 
+
+        int g = -10 ;
+        if ( g >0){
+
+            System.out.println("number is positive ");
+        }
+
+        int h=12;
+        if(h>10 && h<20){
+          System.err.println(" number is in range ");
+
+        }
+         
+          // if else //
+
+          int t=20;
+          if(t>10){
+                  
+                    System.out.println("the number is positive ");
+                   } else{
+                         
+                          System.out.println("number is negative ");
+                          }
+         
+
+            // voting age //
+            
+            int o=18;
+
+            if (o>=18) {
+                     
+                      System.out.println("you can vote");
+                
+            } else{
+                 System.out.println("no you cant vote ");
+            }
+
+// terrenary operator// 
+
+
+
+    }
+
+}
