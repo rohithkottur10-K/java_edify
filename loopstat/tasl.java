7d9e01150d5184631a45096e0eb1aa9d8c49ad3c
rohithkottur10-k <rohithkottur10@gmail.com>
2026-02-05 14:18:10 +0530

    task 150925

---
+++ loopstat/tasl.java
@@ -0,0 +1,34 @@
+package loopstat;
+
+import java.util.Scanner;
+
+public class tasl {
+    public static void main(String[] args) {
+
+        // =====================================================//
+        // collecting data//
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter your name:");
+        String name = input.next();
+        System.out.println("enter your ID:");
+        Scanner rollno = new Scanner(System.in);
+        int roll = input.nextInt();
+        System.out.println("enter your attendence");
+        int attendence=input.nextInt();
+        int A = 65;
+        if (A >= 65) {
+            System.out.println("your are allowed");
+        } else {
+            System.out.println("your are not allowed");
+        }
+        
+  //===============================================================//
+           // asking students for marks input //
+            
+           
+
+
+         
+    }
+}
